b804237c5c6ad2a57c5a66a3f8dfdd2bfac5c099
lfflorian <lfflorian4@gmail.com>
2019-02-18 10:37:13 -0600

    angularjs agreado

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,10 @@
 import '../css/main.scss';
 import 'bootstrap';
 import 'jquery';
+import 'angular';
+
+var app = angular.module('App', []);
+
+app.controller('myCtrl', function($scope) {
+    $scope.firstName= "Prueba de angujarJS";
+  });
